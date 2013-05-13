c38e4952997d95871d777028133a1603f9e511d3
oliyh <git@oliy.co.uk>
2013-05-13 08:39:09 -0700

    oops, forgot to add basket store

---
+++ src/eatme/basket_store.clj
@@ -0,0 +1,28 @@
+(ns eatme.basket-store
+  (:require [eatme.config :refer [config]]
+            [monger.core :as m]
+            [monger.collection :as mc])
+  (:import [org.bson.types ObjectId]
+           [com.mongodb DB WriteConcern]))
+
+;; connect to mongo
+(m/connect-via-uri! (config :mongo-uri) )
+(m/set-db! (m/get-db)) ;; db name is in the uri
+
+(defn- replace-id [id basket]
+  (assoc (dissoc basket :id) :_id id))
+
+(defn save-basket [basket]
+  (let [id (if-let [id (:id basket)] (ObjectId. id) (ObjectId.))]
+    (mc/update "basket" {:_id id} (replace-id id basket) :upsert true)
+    {:id (str id)}))
+
+(defn- replace-_id [basket]
+  (let [id (str (:_id basket))]
+    (assoc (dissoc basket :_id) :id id)))
+
+(defn load-basket [id]
+  (replace-_id (mc/find-map-by-id "basket" (ObjectId. id))))
+
+(defn user-baskets [owner]
+  (map replace-_id (mc/find-maps "basket" {:owner owner})))
