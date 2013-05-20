d8e8901e3252015b8ce6b9da6fa4a287e0c61b8e
oliyh <git@oliy.co.uk>
2013-05-20 04:07:24 -0700

    reading vars from env as well

--- src/eatme/config.clj
+++ src/eatme/config.clj
@@ -1,16 +1,31 @@
 (ns eatme.config
-  (:require [shoreleave.server-helpers :refer [safe-read]]))
+  (:require [clojure.string :as string]
+            [shoreleave.server-helpers :refer [safe-read]]))
 
 (def config-atom (atom {}))
 
-(defn init-config
+(defn- read-config-file
   "Loads config from resources/config/<env>.edn"
+  [config-name]
+  (safe-read (slurp
+              (str "resources/config/" config-name ".edn"))))
+
+(defn read-env-vars []
+  (reduce
+   (fn [env [k v]] (if (.startsWith k "eatme.")
+                    (assoc env (keyword (string/replace-first k "eatme." "")) v)
+                    env))
+   {} (System/getenv)))
+
+(defn init-config
+  "Loads config from file and overrides from system env"
   ([]
      (init-config :dev))
   ([config-name]
-     (reset! config-atom (safe-read (slurp
-                                     (str "resources/config/" (name config-name) ".edn"))))))
+     (let [config-name (name config-name)]
+       (reset! config-atom (merge (read-config-file config-name)
+                                  (read-env-vars))))))
 
-(def config (fn
-              ([] @config-atom)
-              ([& args] (apply @config-atom args))))
+(def config
+  (fn ([] @config-atom)
+    ([& args] (apply @config-atom args))))
