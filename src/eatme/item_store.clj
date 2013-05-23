3909869e8395e34ae77a197353542e6b26ea8863
oliyh <git@oliy.co.uk>
2013-05-23 04:46:17 -0700

    populate now populates everything using upsert, suggest returns top 10 only

--- src/eatme/item_store.clj
+++ src/eatme/item_store.clj
@@ -4,6 +4,7 @@
             [cheshire.generate]
             [monger.core :as m]
             [monger.collection :as mc]
+            [monger.query :as mq]
             [monger.joda-time]
             [eatme.item-library :as library])
   (:import [org.bson.types ObjectId]
@@ -16,18 +17,22 @@
   (m/set-db! (m/get-db))) ;; db name is in the uri
 
 (defn add-item [category item]
-  (mc/insert "item" (assoc item :category category)))
+  (mc/update "item" {:ProductId (:ProductId item)} (assoc item :category category) :upsert true))
 
 (defn- replace-_id [i]
   (let [id (str (:_id i))]
     (assoc (dissoc i :_id) :id id)))
 
 (defn suggest-item [q]
-  (map #(select-keys % [:Name])
-       (mc/find-maps "item" {:Name {$regex (str ".*" q ".*") $options "i"}})))
+  (map replace-_id
+       (mq/with-collection "item"
+         (mq/find {:Name {$regex (str ".*" q ".*") $options "i"}})
+         (mq/fields [:Name])
+         (mq/limit 10))))
 
 ;; currently only does the first 2 (of 500+) categories
 (defn populate-from-library []
-  (doseq [category (take 2 (library/all-shelves))]
+  (doseq [category (library/all-shelves)]
+    (println "Populating category" category)
     (doseq [p (library/products (-> category :shelf :Id))]
       (add-item category p))))
