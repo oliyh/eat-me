ab7606a6bd1d2946cc174eb4a1f4266f988fcc46
Oliver Hine <git@oliy.co.uk>
2014-06-20 23:40:26 +0100

    Implementing basket store very simply

--- src/eatme/basket_store.clj
+++ src/eatme/basket_store.clj
@@ -3,66 +3,37 @@
             [cheshire.generate]
             [monger.core :as m]
             [monger.collection :as mc]
-            [monger.joda-time])
+            [monger.joda-time]
+            [monger.operators :refer :all])
   (:import [org.bson.types ObjectId]
            [com.mongodb DB WriteConcern]))
 
-(defn init
+(defn init!
   "Connect to Mongo"
   []
   (m/connect-via-uri! (config :MONGOLAB_URI))
   (m/set-db! (m/get-db))) ;; db name is in the uri
 
-(defn- replace-id [id basket]
-  (assoc (dissoc basket :id) :_id id))
-
-(defn save-basket [basket]
-  (let [id (if-let [id (:id basket)] (ObjectId. id) (ObjectId.))]
-    (mc/update "basket" {:_id id} (replace-id id basket) :upsert true)
-    {:id (str id)}))
-
-(defn- replace-_id [basket]
-  (let [id (str (:_id basket))]
-    (assoc (dissoc basket :_id) :id id)))
-
 (defn load-basket [id]
-  (replace-_id (mc/find-map-by-id "basket" (ObjectId. id))))
+  (let [basket-id (ObjectId. id)]
+    (assoc (mc/find-map-by-id "basket" basket-id)
+      :items (mc/find-maps "items" {:_basket basket-id}))))
 
 (defn user-baskets [owner]
-  (reverse (sort-by :timestamp
-                    (map replace-_id (mc/find-maps "basket" {:owner owner})))))
+  (reverse (sort-by :timestamp (mc/find-maps "basket" {:owner owner}))))
 
-(defn get-state [a b]
-  (if (not= (:state a) (:state b))
-    "conflict"
-    (:state a)))
+(defn basket-exists? [id]
+  (nil? (load-basket id)))
 
-(defn normalise-basket [{:keys [items] :as basket}]
-  (assoc basket :items
-         (for [item-group (vals (group-by :item-name items))]
-           (reduce (fn [o n]
-                     (assoc o :qty (+ (:qty o) (:qty n))
-                            :state (get-state o n)))
-                   (first item-group) (rest item-group)))))
+(defn create-basket! []
+  (let [id (ObjectId.)]
+    (mc/insert "basket" {:_id id})
+    (str id)))
 
-(def states {"list" 0
-             "basket" 1
-             "conflict" 2})
+(defn add-item! [basket-id item]
+  (let [id (ObjectId. basket-id)]
+    (mc/insert "items" (merge {:_id id :_basket (ObjectId. basket-id)} item))
+    (str id)))
 
-(defn greater-state [a b]
-  (condp some [(:state a) (:state b)]
-    #(= "conflict" %) "conflict"
-    #(= "basket" %) "basket"
-    "list"))
-
-(defn merge-baskets [a b]
-  (let [all-a-items (group-by :item-name (:items (normalise-basket a)))
-        all-b-items (group-by :item-name (:items (normalise-basket b)))]
-    {:items
-     (for [item-name (keys all-a-items)
-           :let [a-items (get all-a-items item-name)
-                 b-items (get all-b-items item-name)]]
-       (cond
-        (every? #(= (:qty (first a-items)) (:qty %)) (concat (rest a-items) b-items))
-        (reduce (fn [o n] (assoc o :state (greater-state o n)))
-                (first a-items) (concat (rest a-items) b-items))))}))
+(defn delete-basket! [id]
+  (mc/remove "basket" {:_id (ObjectId. id)}))
